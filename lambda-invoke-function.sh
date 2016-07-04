93a9aef015b9642760124e74e22d385dfed4e7d6
Jussi.Heinonen <jussi.heinonen@ft.com>
2016-07-04 16:04:33 +0100

    Replace sed with grep and few pipes

--- lambda-invoke-function.sh
+++ lambda-invoke-function.sh
@@ -12,14 +12,14 @@ source functions.sh
 function invokeFunction() {
     if [[ "${SETTINGS[AWS_LAMBDA_DRYRUN]}" ]]; then
         aws lambda invoke  --function-name ${SETTINGS[AWS_LAMBDA_FUNCTION]} --invocation-type DryRun ${OUTPUT}
-        if [[ "$(sed -En 's/*\"StatusCode\"://p' output.txt  | tr -d ' ')" -ne "204" ]]; then
+        if [[ "$(grep StatusCode output.txt | tr -d ' ' | cut -d ':' -f 2)" -ne "204" ]]; then
             return 1
         else
             echo -e "\e[31mStatusCode 204: success\e[0m"
         fi             
     else
         aws lambda invoke  --function-name ${SETTINGS[AWS_LAMBDA_FUNCTION]} ${OUTPUT}
-        if [[ "$(sed -En 's/*\"StatusCode\"://p' output.txt  | tr -d ' ')" -ne "200" ]]; then
+        if [[ "$(grep StatusCode output.txt | tr -d ' ' | cut -d ':' -f 2)" -ne "200" ]]; then
             return 1
         else
             echo -e "\e[31mStatusCode 200: success\e[0m"
