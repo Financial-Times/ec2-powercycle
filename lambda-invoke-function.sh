003933fcdaa00575b1649ae50667f92a2975974e
Jussi.Heinonen <jussi.heinonen@ft.com>
2016-07-04 13:36:20 +0100

    Add lambda-invoke-function.sh and update circle.yml

--- lambda-invoke-function.sh
+++ lambda-invoke-function.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-# Script creates a new version of the $LATEST Lambda job
-# Lambda alias can then be linked to specific version 
-# More information: http://docs.aws.amazon.com/cli/latest/reference/lambda/publish-version.html
+# Script executes Lambda function 
 #
 # USAGE:
-# ./lambda-publish-version.sh
+# ./lambda-invoke-function.sh
 
 source functions.sh
 
-function publishVersion() {
-    aws lambda publish-version  --function-name ${SETTINGS[AWS_LAMBDA_FUNCTION]}
+function invokeFunction() {
+    aws lambda invoke  --function-name ${SETTINGS[AWS_LAMBDA_FUNCTION]}
 }
+
+if [[ "$1" == "DryRun" ]]; then
+    echo -e "\e[31mINvoking function ${SETTINGS[AWS_LAMBDA_FUNCTION]} in DryRun mode. Exit 0.\e[0m"
+    exit 0
+fi
 
 test -z ${AWS_ACCESS_KEY_ID} && promptUser "Enter AWS_ACCESS_KEY_ID" AWS_ACCESS_KEY_ID || SETTINGS[AWS_ACCESS_KEY_ID]="${AWS_ACCESS_KEY_ID}"
 test -z ${AWS_SECRET_ACCESS_KEY} && promptUser "Enter AWS_SECRET_ACCESS_KEY" AWS_SECRET_ACCESS_KEY || SETTINGS[AWS_SECRET_ACCESS_KEY]="${AWS_SECRET_ACCESS_KEY}"
@@ -22,4 +25,4 @@ echo -e "\e[31mExporting settings\e[0m"
 exportSettings
 echo -e "\e[31mVerifying Lambda function\e[0m"
 verifyLambdaFunction && echo -e "\e[31mLambda function access OK\e[0m" || errorAndExit "Failed to access function ${SETTINGS[AWS_LAMBDA_FUNCTION]}" 1
-publishVersion && echo -e "\e[31mNew version created successfully\e[0m" || errorAndExit "Failed to create new version for ${SETTINGS[AWS_LAMBDA_FUNCTION]}" 1
+invokeFunction && echo -e "\e[31mLambda function ${SETTINGS[AWS_LAMBDA_FUNCTION]} executed successfully\e[0m" || errorAndExit "Failed to run Lambda function ${SETTINGS[AWS_LAMBDA_FUNCTION]}" 1
