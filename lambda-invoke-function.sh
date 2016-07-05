e2a787d2a876422b84471d6aef9e01ed96db6917
Jussi.Heinonen <jussi.heinonen@ft.com>
2016-07-05 10:41:07 +0100

    Replace --invication-type with  --payload

--- lambda-invoke-function.sh
+++ lambda-invoke-function.sh
@@ -12,7 +12,7 @@ source functions.sh
 
 function invokeFunction() {
     if [[ "${SETTINGS[AWS_LAMBDA_DRYRUN]}" ]]; then
-        aws lambda invoke  --function-name ${SETTINGS[AWS_LAMBDA_FUNCTION]} --invocation-type DryRun ${OUTPUT}
+        aws lambda invoke  --function-name ${SETTINGS[AWS_LAMBDA_FUNCTION]} --payload DryRun ${OUTPUT}
         return $?           
     else
         aws lambda invoke  --function-name ${SETTINGS[AWS_LAMBDA_FUNCTION]} ${OUTPUT}
