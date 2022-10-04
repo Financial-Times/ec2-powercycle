82133c932ad52c559695a05c1d553f1152b4c9e9
Ivan Ivanov <ivan.ivanov@ft.com>
2022-10-04 13:35:35 +0300

    Fix CI/CD and delete unused bash files

--- sh/package.sh
+++ sh/package.sh
@@ -5,9 +5,16 @@
 ROOT_DIR="ec2-powercycle"
 CLONE_URL="https://github.com/Financial-Times/ec2-powercycle.git"
 BUILD_INFO="build.info"
+BRANCH="${CIRCLE_BRANCH}"
+
+if [[ -z "${BRANCH}" ]]
+then
+    BRANCH="${CIRCLE_TAG}"
+fi
+
 
 test -d ${ROOT_DIR} && rm -rf ${ROOT_DIR}
-git clone ${CLONE_URL} ${ROOT_DIR} && echo -e "\e[31mRepository cloned \e[0m" || exit 1
+git clone -b "${BRANCH}" ${CLONE_URL} ${ROOT_DIR} && echo -e "\e[31mRepository cloned \e[0m" || exit 1
 cd ${ROOT_DIR}
 pip3 install -r requirements.txt -t lib/ && echo -e "\e[31mDependencies installed \e[0m" || exit 1
 echo -e "Build: ${CIRCLE_BUILD_URL}\\nRepository: ${CIRCLE_REPOSITORY_URL}" > ${BUILD_INFO}
